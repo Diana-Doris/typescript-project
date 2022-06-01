9a44f0d239ec575a71644670cd072ca809487516
Diana-Doris <15637298867@163.com>
2022-06-01 18:39:38 +0800

    commit

--- diana-self-study-test-proj/src/PokerGames/index.ts
+++ diana-self-study-test-proj/src/PokerGames/index.ts
@@ -1,4 +1,16 @@
-import { createDeck, printDeck } from './func'
+// import { createDeck, printDeck } from './func'
 
-const deck = createDeck()
-printDeck(deck)
+import { Deck } from './deck'
+
+// const deck = createDeck()
+// printDeck(deck)
+
+const deck = new Deck()
+deck.shuffle()
+deck.print()
+const result = deck.publish()
+
+console.log(result.left.print())
+console.log(result.player1.print())
+console.log(result.player2.print())
+console.log(result.player3.print())
--- diana-self-study-test-proj/src/index.ts
+++ diana-self-study-test-proj/src/index.ts
@@ -1,152 +1,38 @@
-interface Condition {
-  (n: number, i: number): boolean
-  // 大括号里面没有任何的成员名称的时候，表示这个大括号只是一个定界符而已
-}
+class User {
+  readonly id: number //不能修改
+  // name: string
+  // age: number
+  gender: '男' | '女' = '男'
+  pid?: string
+  private _publishNumber: number = 3 // 每天一共发布多少篇文章数量
+  private _currentNumber: number = 0 // 每天可以多少篇文章数量
 
-function sum(numbers: number[], callBack: Condition): number {
-  let s = 0
-  numbers.forEach((n, i) => {
-    if (callBack(n, i)) {
-      s += n
+  constructor(private _name: string, private _age: number) {
+    // 默认值 性别
+    // this.name = name
+    // this.age = age
+    this.id = Math.random()
+    // this.gender = gender
+  }
+
+  set age(value: number) {
+    this._age = value
+  }
+
+  get age() {
+    return this._age
+  }
+
+  publish(title: string) {
+    if (this._currentNumber < this._publishNumber) {
+      console.log('发布一篇文章', title)
+      this._currentNumber++
+    } else {
+      console.log('今日不能发布了～')
     }
-  })
-  return s
+  }
 }
 
-// const result = sum([3, 4, 5, 6, 7, 8], (n) => n % 2 !== 0)//不报错  参数不能多  但是可以少
-const result = sum([3, 4, 5, 6, 7, 8], (n, i) => i % 2 !== 0)
-
-// interface User {
-//   name?: string //?可选
-//   age: number
-// }
-// let obj = {
-//   age: 11,
-//   newVal: 'gaga',
-// }
-// let u: User = obj //ok
-
-// let u: User = {
-//   age: 11,
-// }//ok
-
-// let u: User = {
-//     age: 11,
-//     newVal:'gaga'
-// }// false
-
-// interface Duck {
-//   sound: 'gagaga'
-//   swin(): void
-// }
-
-// let person = {
-//   name: 'kevin',
-//   age: 12,
-//   sound: 'gagaga' as 'gagaga',
-//   swin: () => {
-//     console.log('gagaga')
-//   },
-// }
-
-// let duck: Duck = person
-
-// 假设有个函数，用于得到服务器某个接口的返回结果，是一个用户对象
-// interface response {
-//   loginID: string
-//   name: string
-//   gender: '男' | '女'
-// }
-
-// // 类型别名
-// // type User = {
-// //   name: string
-// //   age: number
-// // }
-// // 接口定义
-// interface User {
-//   name: string
-//   age: number
-//   //   sayHello: () => void //是一个函数 函数没有参数 返回参数是一个void
-//   sayHello(): void //是一个函数 函数没有参数 返回参数是一个void
-// }
-
-// // 接口定义和类型别名，在约束类中是存在区别的
-// // 约束对象的时候，建议使用接口的方式
-
-// let u: User = {
-//   name: 'kevin',
-//   age: 22,
-//   sayHello() {
-//     console.log('aaa')
-//   },
-// }
-
-// type Condition = (n: number) => boolean
-// interface Condition {
-//   (n: number): boolean
-//   // 大括号里面没有任何的成员名称的时候，表示这个大括号只是一个定界符而已
-// }
-
-// function sum(numbers: number[], callBack: Condition): number {
-//   let s = 0
-//   numbers.forEach((n) => {
-//     if (callBack(n)) {
-//       s += n
-//     }
-//   })
-//   return s
-// }
-
-// const result = sum([3, 4, 5, 6, 7, 8], (n) => n % 2 !== 0)
-// console.log(result)
-
-// interface A {
-//   T1: string
-// }
-// interface B {
-//   T2: number
-// }
-
-// interface C extends A, B {
-//   T3: string
-// }
-// type A = {
-//   T1: string
-// }
-// type B = {
-//   T2: number
-// }
-// type C = {
-//   T3: string
-// } & A &
-//   B
-// const obj: C = {
-//   T2: 99,
-//   T1: '99',
-//   T3: '00',
-// }
-
-// interface User {
-//   readonly id: string
-//   name: string
-//   age: number
-//   readonly arr: string[]
-// }
-
-// let U: User = {
-//   id: '5',
-//   name: 'kevin',
-//   age: 12,
-//   arr: ['00'],
-// }
-
-// U.arr.push('00')
-
-// // U.id = '99' //可以用readonly进行只读限制,这样id就不能修改了
-
-// let arr: readonly number[] = [3, 4, 5]
-
-// let arr: ReadonlyArray<number> = [3, 4, 5]
-
-// arr.push 加上了readonly  数组中改变函数，都不见了
+const u = new User('a', 1)
+u.age = 1
+console.log(u.age)
